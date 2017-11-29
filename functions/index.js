8b013b4011d81160845a14a73c0fbab3a1318a7f
Brett Bennett <bbennett905@gmail.com>
2017-11-28 19:53:45 -0500

    Add some database rules for some other data

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,16 @@
+// ----- Firebase Cloud Functions -----
+// Purpose of these, for this project, is to work with the database
+//	in a way to ensure that the DB is secure and users can't access
+//	other people's docs.
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-//possible way to do this - user doesnt write, but when a user is added to a doc
-// use firebase functions to add that doc to the user's table entry
-// something like: - won't work for creating new doc, only adding users!
-//    on db write to /docs/$docid/users/
-//      foreach child of node
-//        /users/ child.key /$docid = child.val
-// may need to instead, have another 'table': 'newdocs'
-//  entrys indexed as if array, has userid: true
-//  allows anyone to write to it, and when functions sees a write to it:
-//    create doc and add to users table: child(/$userid)
-//  in client, listen for uid docs list change then open the new doc
-//using both of those together should work
-//needs users set to allow write from functions only
-
+/**
+ * Called when a user requests to create a new doc by writing to the
+ * 	newDocs/ table. Then, creates a new doc and gives the user access
+ */
 exports.onNewDoc = functions.database.ref('/newDocs/{uid}').onCreate(event => {
 	const uid = event.params.uid;
 	console.log("onNewDoc: uid=" + uid);
@@ -30,22 +25,17 @@ exports.onNewDoc = functions.database.ref('/newDocs/{uid}').onCreate(event => {
 	//	Remove the newdocs entry
 	return admin.database().ref('docs').once('value', function (snapshot) {
 			snapshot.forEach(function(snapshot) {
-				//TODO is val actually a string
 				var id = snapshot.key;
 				if (typeof id === 'string' || id instanceof String) {
 					id = parseInt(id);
 				}
 				
-				console.log("onNewDoc: this ss.key=" + snapshot.key);
-				console.log("onNewDoc: this ss.val=" + snapshot.val());
-				console.log("onNewDoc: this id=" + id);
 				if (id > largestId) {
-					console.log("onNewDoc: id is greater");
 					largestId = id;
 				}
 			});
 			newId = largestId + 1;
-			console.log("onNewDoc: newId=" + newId);
+			console.log("onNewDoc: Creating new doc with id = " + newId);
 		}).then(function(s) {
 			return Promise.all([
 				admin.database().ref('docs/' + newId + '/users/' + uid).set(true),
@@ -57,12 +47,11 @@ exports.onNewDoc = functions.database.ref('/newDocs/{uid}').onCreate(event => {
 
 /**
  * Called when a new user is added to an existing doc - 
- *  adds that user to the users table
+ *  adds that doc to the users table, allowing them to access it
  */
 exports.onAddUser = functions.database.ref('/docs/{docid}/users/{uid}').onWrite(event => {
 	const val = event.data.val();
 	const uid = event.data.key;
-	console.log("onAddUser: val=" + val + ", uid=" + uid);
-	//TODO this may be horribly wrong
+
 	return admin.database().ref('users/' + uid + '/' + event.params.docid).set(val);
 });
