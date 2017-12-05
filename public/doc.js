e9deb913331b47318c5462842211b5e3c49fe196
Unknown <bbennett905@gmail.com>
2017-12-05 12:40:12 -0500

    Add doc.html/js to display doc in

---
+++ public/doc.js
@@ -0,0 +1,48 @@
+//TODO when refreshing after login, everything is hidden
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+
+var db = firebase.database();
+
+document.addEventListener('DOMContentLoaded', function() {
+	// // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+	// // The Firebase SDK is initialized and available here!
+	if (!isLoggedIn) {
+		document.getElementById('logged-in').style.display = 'none';
+	}
+});
+
+var isLoggedIn = false;
+/**
+ * Displays the UI for a signed in user.
+ * @param {!firebase.User} user
+ */ 
+var handleSignedInUser = function(user) {
+	isLoggedIn = true;
+	document.getElementById('logged-in').style.display = 'block';
+	document.getElementById('logged-out').style.display = 'none';
+
+	document.getElementById('user-name').innerHTML = user.displayName;
+	document.getElementById('user-email').innerHTML = user.email;
+	$('#sign-out').on('click', function() {
+		console.log("sign out");
+		firebase.auth().signOut();
+	});
+};
+
+/**
+ * Displays the UI for a signed out user.
+ */
+var handleSignedOutUser = function() {
+	isLoggedIn = false;
+	document.getElementById('logged-in').style.display = 'none';
+	document.getElementById('logged-out').style.display = 'block';
+
+	document.getElementById('docslist').innerHTML = "Loading your docs...";
+	document.getElementById('docslist').style.display = 'none';
+};
+
+// Listen to change in auth state so it displays the correct UI for when
+// the user is signed in or not.
+firebase.auth().onAuthStateChanged(function(user) {
+	user ? handleSignedInUser(user) : handleSignedOutUser();
+});
