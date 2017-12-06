5340a281bb1c783617f60054f4d68675b4e38c9d
Unknown <bbennett905@gmail.com>
2017-12-06 10:52:30 -0500

    Add ability to add user stories, only show titles and notes so far

--- public/doc.js
+++ public/doc.js
@@ -9,6 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
 	// // The Firebase SDK is initialized and available here!
 	if (!isLoggedIn) {
 		document.getElementById('logged-in').style.display = 'none';
+		document.getElementById('logged-out').style.display = 'block';
+	} else {
+		document.getElementById('logged-in').style.display = 'block';
+		document.getElementById('logged-out').style.display = 'none';
 	}
 	docid = parseInt(window.location.hash.substring(1));
 	if (isNaN(docid)) {
@@ -77,20 +81,34 @@ document.addEventListener('DOMContentLoaded', function() {
 			}
 		});
 		document.getElementById('people-add-btn').onclick = function() {
-			console.log("button");
-			var count = 0;
+			var max = 0;
 			firebase.database().ref('docs/' + docid + '/people').once('value', function(snapshot) {
 				snapshot.forEach(function(innerSnap) {
-					count++;
+					if (parseInt(innerSnap.key) > max) {
+						max = parseInt(innerSnap.key);
+					}
 				});
-				//count is the new id to use
-				firebase.database().ref('docs/' + docid + '/people/' + count + '/name').set("Name");
-				firebase.database().ref('docs/' + docid + '/people/' + count + '/email').set("test@example.com");
-				firebase.database().ref('docs/' + docid + '/people/' + count + '/phone').set("867-5309");
+				max++;
+				//max is the new id to use
+				firebase.database().ref('docs/' + docid + '/people/' + max + '/name').set("Name");
+				firebase.database().ref('docs/' + docid + '/people/' + max + '/email').set("test@example.com");
+				firebase.database().ref('docs/' + docid + '/people/' + max + '/phone').set("867-5309");
 			});
 		};
 		firebase.database().ref('docs/' + docid + '/people').once('value', buildPeopleTable);
-		
+		document.getElementById('userstories-add-btn').onclick = function() {
+			var max = 0;
+			firebase.database().ref('docs/' + docid +'/userStories').once('value', function(snapshot) {
+				snapshot.forEach(function(innerSnap) {
+					if (parseInt(innerSnap.key) > max) {
+						max = parseInt(innerSnap.key);
+					}
+				});
+				max++;
+				firebase.database().ref('docs/' + docid + '/userStories/' + max + '/title').set("Unnamed User Story");
+			});
+		};
+		firebase.database().ref('docs/' + docid + '/userStories').once('value', buildUserStoriesTable);
 	}
 });
 
@@ -119,9 +137,6 @@ var handleSignedOutUser = function() {
 	isLoggedIn = false;
 	document.getElementById('logged-in').style.display = 'none';
 	document.getElementById('logged-out').style.display = 'block';
-
-	document.getElementById('docslist').innerHTML = "Loading your docs...";
-	document.getElementById('docslist').style.display = 'none';
 };
 
 // Listen to change in auth state so it displays the correct UI for when
@@ -162,6 +177,7 @@ function scrumMaster(snapshot) {
 	document.getElementById("doc-intro-master").appendChild(para);
 }
 
+var isPeopleUpdateRegistered = false;
 function buildPeopleTable(snapshot) {
 	var people = document.getElementById('peoplelist');
 	people.innerHTML = "";
@@ -249,24 +265,26 @@ function buildPeopleTable(snapshot) {
 	if (count == 0) {
 		people.innerHTML = "No one's on this team, add someone!";
 	}
-
-	firebase.database().ref('docs/' + docid + '/people').on('value', updatePeopleTable);
+	if (!isPeopleUpdateRegistered) {
+		firebase.database().ref('docs/' + docid + '/people').on('value', updatePeopleTable);
+		isPeopleUpdateRegistered = true;
+	}
 }
 
-var lastSize = 0;
+var lastPeopleSize = 0;
 
 function updatePeopleTable(snapshot) {
 	var count = 0;
 	snapshot.forEach(function(innerSnap) {
 		count++;
 	});
-	if (count != lastSize) {
+	if (count != lastPeopleSize) {
 		//Need to rebuild table
-		lastSize = count;
+		lastPeopleSize = count;
 		buildPeopleTable(snapshot);
 	} else {
 		//Length is same, so no need to rebuild
-		lastSize = count;
+		lastPeopleSize = count;
 		snapshot.forEach(function(innerSnap) {
 			var n = document.getElementById("p-n-" + innerSnap.key);
 			n.value = innerSnap.child('name').val();
@@ -276,4 +294,125 @@ function updatePeopleTable(snapshot) {
 			p.value = innerSnap.child('phone').val();
 		});
 	}
+}
+
+var isUserStoriesUpdateRegistered = false;
+function buildUserStoriesTable(snapshot) {
+	var list = document.getElementById('userstories-list');
+	list.innerHTML = "";
+	//TODO progress bar for each US?
+	snapshot.forEach(function(innerSnap) {
+		//Title, notes, tasks, acceptance criteria
+		var div = document.createElement('div');
+
+		var title = document.createElement('h4');
+		var titleInput = document.createElement("input");
+		titleInput.id = "us-title-" + innerSnap.key;
+		titleInput.classList.add("user-input");
+		titleInput.ondblclick = function() {
+			this.readOnly = false;
+		};
+		titleInput.onblur = function() {
+			this.readOnly = true;
+		};
+		titleInput.placeholder = "No title";
+		titleInput.value = innerSnap.child('title').val();
+		titleInput.oninput = function () {
+			firebase.database().ref('docs/' + docid + '/userStories/' + innerSnap.key + '/title').set(this.value);
+		};
+		title.appendChild(titleInput);
+		div.appendChild(title);
+
+		var notesPanel = document.createElement('div');
+		notesPanel.classList.add("panel");
+		notesPanel.classList.add("panel-default");
+		var notesHead = document.createElement('div');
+		notesHead.classList.add("panel-heading");
+		notesHead.innerHTML = "Notes";
+		var notesBody = document.createElement('div');
+		notesBody.classList.add("panel-body");
+		var notesInput = document.createElement('textarea');
+		notesInput.classList.add("user-input");
+		notesInput.id = "us-notes-" + innerSnap.key;
+		notesInput.placeholder = "Any notes, links, references can be saved here";
+		notesInput.datatype = "text";
+		notesInput.readonly = true;
+		notesInput.ondblclick = function() {
+			this.readOnly = false;
+		};
+		notesInput.onblur = function() {
+			this.readOnly = true;
+		};
+		notesInput.value = innerSnap.child('notes').val();
+		notesInput.oninput = function () {
+			firebase.database().ref('docs/' + docid + '/userStories/' + innerSnap.key + '/notes').set(this.value);
+		};
+		notesBody.appendChild(notesInput);
+		notesPanel.appendChild(notesHead);
+		notesPanel.appendChild(notesBody);
+		div.appendChild(notesPanel);
+		list.appendChild(div);
+	});
+	
+	if (!isUserStoriesUpdateRegistered) {
+		firebase.database().ref('docs/' + docid + '/userStories').on('value', updateUserStoriesTable);
+		isUserStoriesUpdateRegistered = true;
+	}
+}
+
+function updateUserStoriesTable(snapshot) {
+	if (isUserStorySnapshotChanged(snapshot)) {
+		buildUserStoriesTable(snapshot);
+	} else {
+		snapshot.forEach(function(innerSnap) {
+			var title = document.getElementById("us-title-" + innerSnap.key);
+			title.value = innerSnap.child('title').val();
+			var title = document.getElementById("us-notes-" + innerSnap.key);
+			title.value = innerSnap.child('notes').val();
+		});
+	}
+}
+
+var lastUserStoriesSize = 0;
+var lastTasksSize = [];
+var lastCriteriaSize = [];
+function isUserStorySnapshotChanged(snapshot) {
+	var count = 0;
+	var tasksCountList = [];
+	var criteriaCountList = [];
+	snapshot.forEach(function(innerSnap) {
+		count++;
+		var taskCount = 0;
+		innerSnap.child('tasks').forEach(function(snap) {
+			taskCount++;
+		});
+		tasksCountList[count] = taskCount;
+		var criteriaCount = 0;
+		innerSnap.child('acceptanceCriteria').forEach(function(snap) {
+			criteriaCount++;
+		});
+		criteriaCountList[count] = criteriaCount;
+	});
+	var isDifferent = false;
+	if (lastUserStoriesSize != count || 
+		lastTasksSize.length != tasksCountList.length ||
+		lastCriteriaSize.length != criteriaCountList.length) {
+		isDifferent = true;
+	} else {
+		for (var i = 0; i < lastTasksSize.length; i++) {
+			if (lastTasksSize[i] != tasksCountList[i]) {
+				isDifferent = true;
+			}
+		}
+		for (var i = 0; i < lastCriteriaSize.length; i++) {
+			if (lastCriteriaSize[i] != criteriaCountList[i]) {
+				isDifferent = true;
+			}
+		}
+	}
+
+	lastTasksSize = tasksCountList;
+	lastUserStoriesSize = count;
+	lastCriteriaSize = criteriaCountList;
+	return isDifferent;
 }
